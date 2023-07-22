268b58bf95b1c87136a48b4f444864220ae0c6b6
Loïc Ballanger <ballanger.loic@gmail.com>
2023-07-22 23:06:09 +0200

    Change of homepage with the choice of joining several rooms

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { joinRoom, socketConnection } from "../../services/slices/gameSlice";
@@ -6,8 +7,21 @@ function Home() {
   const dispatch = useDispatch();
 
   const handleClick = () => {
+    if (!selectedRoom) return;
     dispatch(socketConnection("true"));
-    dispatch(joinRoom("abc"));
+    dispatch(joinRoom(selectedRoom));
+  };
+
+  const [selectedRoom, setSelectedRoom] = useState<string | null>(null);
+
+  const rooms = [
+    { name: "Salle 1", capacity: "0/100", path: "1" },
+    { name: "Salle 2", capacity: "0/100", path: "2" },
+    { name: "Salle 3", capacity: "0/100", path: "3" },
+  ];
+
+  const handleRoomSelection = (path: string) => {
+    setSelectedRoom(path);
   };
 
   return (
@@ -18,12 +32,47 @@ function Home() {
         cinq de vos symboles (X ou O) en ligne verticale, horizontale ou
         diagonale. Le premier joueur à accomplir cela remporte la partie !
       </p>
+      <div className="border rounded-lg p-4 mb-8">
+        <h2 className="text-xl font-bold mb-4">Choisissez une salle</h2>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center mb-4">
+            <ul className="flex flex-col items-center justify-center">
+              {rooms.map((room) => (
+                <li
+                  key={room.name}
+                  className={`flex flex-row items-center justify-between rounded-s-lg my-1  border border-white cursor-pointer ${
+                    selectedRoom === room.path ? " rounded-e-lg border" : ""
+                  }`}
+                  style={
+                    selectedRoom === room.path
+                      ? { border: "1px solid blue", backgroundColor: "#f0f8ff" }
+                      : {}
+                  }
+                  onClick={() => handleRoomSelection(room.path)}
+                >
+                  <span
+                    className={`mx-2 ${selectedRoom === room.path ? "" : ""}`}
+                  >{`Salle ${room.path}`}</span>
+                  <span
+                    className={`px-2 py-1 bg-green-500 text-white font-semibold rounded-e-lg ${
+                      selectedRoom === room.path ? "" : ""
+                    }`}
+                  >
+                    {room.capacity}
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+
       <Link
-        to="/gomoku"
+        to={selectedRoom ? `/gomoku/room/${selectedRoom}` : "#"}
         className="px-4 py-2 bg-blue-500 text-white font-semibold rounded shadow"
         onClick={handleClick}
       >
-        Commencer le jeu
+        Rejoindre
       </Link>
     </div>
   );
