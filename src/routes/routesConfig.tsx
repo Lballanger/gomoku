11ddf2b5ad7f14a32ccf26757980804d81385dc1
Loïc Ballanger <ballanger.loic@gmail.com>
2023-07-23 00:24:29 +0200

    Modification of the route for rooms

--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -1,7 +1,7 @@
 import PrivateRoute from "./PrivateRoute";
 import Home from "../pages/Home/Home";
 import ErrorPage from "../pages/Error/Error";
-import Gomoku from "../pages/Gomoku/Gomoku";
+import Room from "../pages/Room/Room";
 
 const routesConfig = [
   {
@@ -10,8 +10,8 @@ const routesConfig = [
     errorElement: <ErrorPage />,
   },
   {
-    path: "/gomoku",
-    element: <Gomoku />,
+    path: "/gomoku/room/:id",
+    element: <Room />,
   },
   {
     element: <PrivateRoute />,
