bf19edfdbc1408be1bcbee0ca2a3d70029d0a07a
Loïc Ballanger <ballanger.loic@gmail.com>
2023-09-30 15:30:01 +0200

    Add chatSocketMiddleware

---
+++ src/middlewares/chatSocketMiddleware.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Dispatch } from "redux";
+import { RootState } from "../services/store";
+
+interface SocketMiddlewareParams {
+  dispatch: Dispatch;
+  getState: () => RootState;
+}
+
+export default function chatSocketMiddleware(socket: any) {
+  return (params: SocketMiddlewareParams) => (next: any) => (action: any) => {
+    const { dispatch } = params;
+    const { type, payload } = action;
+
+    switch (type) {
+      case "game/joinRoom": {
+        socket.on("receivedMessageInRoom", (messages: string[][]) => {
+          dispatch({ type: "game/receivedMessageInRoom", payload: messages });
+        });
+
+        socket.on("receivedMessageInGame", (message: string) => {
+          dispatch({
+            type: "game/receivedMessageInGame",
+            payload: message,
+          });
+        });
+        break;
+      }
+
+      case "game/sentMessageInRoom": {
+        socket.emit("sentMessageInRoom", payload);
+        break;
+      }
+
+      case "game/sentMessageInGame": {
+        socket.emit("sentMessageInGame", payload);
+        break;
+      }
+    }
+    return next(action);
+  };
+}
--- src/services/store.ts
+++ src/services/store.ts
@@ -5,6 +5,7 @@ import { GameSlice } from "./slices/gameSlice";
 
 import SocketClient from "../socket/SocketClient";
 import socketMiddleware from "../middlewares/socketMiddleware";
+import chatSocketMiddleware from "../middlewares/chatSocketMiddleware";
 
 const socket = new SocketClient();
 
@@ -18,7 +19,9 @@ export type RootState = ReturnType<typeof rootReducers>;
 const store = configureStore({
   reducer: rootReducers,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(socketMiddleware(socket)),
+    getDefaultMiddleware()
+      .concat(socketMiddleware(socket))
+      .concat(chatSocketMiddleware(socket)),
 });
 
 export default store;
